3bff4757c77a7267bc1c595c85463735ff5d9ec9
qasimgit <qasimpkkhi@gmail.com>
2020-09-01 17:01:25 +0500

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
-   
+   <div>
+       Qasim
+   </div>
   );
 }
 
